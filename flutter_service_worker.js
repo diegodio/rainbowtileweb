713fe68e2d521b6230702dec80cf238ce35af8ab
diegodio <diego__dio@hotmail.com>
2020-11-15 23:03:32 -0300

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,9 +11,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "568ef4cbdaab87b255c9d05e1fa3dc49",
-"/": "568ef4cbdaab87b255c9d05e1fa3dc49",
-"main.dart.js": "980837a869ea6c81e38e59c1813835ba",
+"index.html": "059acdd873d195291d66919cef8a7860",
+"/": "059acdd873d195291d66919cef8a7860",
+"main.dart.js": "08561d00190e782a1b492b5ca410c29d",
 "manifest.json": "f6c19a61a240bfdca312e3a649cb92b0",
 "version.json": "4948c14e3f6b1d66f0baceab9cae729b"
 };
